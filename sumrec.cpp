d13bafe45dd0a2f6449494fdd964164d8ed8105b
YashBansod123 <144675161+YashBansod123@users.noreply.github.com>
2024-07-12 16:25:52 +0530

    Add files via upload
    complete website

---
+++ sumrec.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+void print(int arr[], int n){
+    cout<<"size of array is"<< n<<endl;
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+}
